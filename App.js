8417c3bd5de6d6cfe6ca105a2163b72f33a8c603
Mubashir1292 <mubashirliaqat72@gmail.com>
2024-10-07 17:27:25 +0500

    end of project

---
+++ App.js
@@ -0,0 +1,11 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import {styled} from 'nativewind';
+function App() {
+  return (
+    <React.Fragment>
+      <StyledView className="flex flex-row">
+        <StyledText className="text-black">Mubsahir is writing</StyledText>
+      </StyledView>
+    </React.Fragment>
+  );
+}
